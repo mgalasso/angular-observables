cccce21dc44d8834c687d73d3b1fcd5e77e6ca15
mgalasso <mgalasso@users.noreply.github.com>
2023-02-03 12:54:18 -0800

    2 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BeginComponent } from './begin/begin.component';
 
 import { ImproveComponent } from './improve/improve.component';
+import { SandboxComponent } from './sandbox/sandbox.component';
 import { SelectFiveComponent } from './select/select-five/select-five.component';
 import { SelectFourComponent } from './select/select-four/select-four.component';
 import { SelectOneComponent } from './select/select-one/select-one.component';
@@ -11,7 +12,7 @@ import { SelectTwoComponent } from './select/select-two/select-two.component';
 import { TemplateSwapComponent } from './template-swap/template-swap.component';
 
 export const routes: Routes = [
-  { path: '', component: ImproveComponent },
+  { path: '', component: SandboxComponent },
   { path: 'begin', component: BeginComponent },
   { path: 'improve', component: ImproveComponent },
   { path: 'selectone', component: SelectOneComponent },
@@ -20,6 +21,7 @@ export const routes: Routes = [
   { path: 'selectfour', component: SelectFourComponent },
   { path: 'selectfive', component: SelectFiveComponent },
   { path: 'swap', component: TemplateSwapComponent },
+  { path: 'sand', component: SandboxComponent },
 ];
 
 @NgModule({
