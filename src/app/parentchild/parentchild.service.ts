5d3393523089faacdc429e335fa141b89d22d2bf
mgalasso <mgalasso@users.noreply.github.com>
2023-02-06 12:00:31 -0800

    3 files added

---
+++ src/app/parentchild/parentchild.service.ts
@@ -0,0 +1,62 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  catchError,
+  combineLatest,
+  map,
+  Observable,
+  of,
+  Subject,
+  switchMap,
+  throwError,
+} from 'rxjs';
+export interface wafer {
+  id: number;
+  name: string;
+}
+
+@Injectable()
+export class ParentchildService {
+  wafers = [
+    { id: 0, name: 'wafer 1' },
+    { id: 1, name: 'wafer 2' },
+    { id: 2, name: 'wafer 2' },
+  ];
+
+  suppliers = [
+    { id: 0, name: 'supplier 1', waferId: 1 },
+    { id: 1, name: 'supplier 2', waferId: 1 },
+    { id: 2, name: 'supplier 3', waferId: 1 },
+  ];
+
+  wafers$ = of(this.wafers);
+  suppliers$ = of(this.suppliers);
+
+  private waferSelectedSubject = new Subject<number>();
+  waferSelectedAction$ = this.waferSelectedSubject.asObservable();
+
+  // shape on action pattern
+  // selectedWafer$ = combineLatest([
+  //   this.wafers$,
+  //   this.waferSelectedAction$,
+  // ]).pipe(
+  //   map(([wafers, selectedWaferId]) =>
+  //     wafers.find((w) => w.id === selectedWaferId)
+  //   )
+  // );
+
+  // retrieve related data pattern
+  selectedWafer$ = this.waferSelectedAction$.pipe(
+    switchMap((id) =>
+      this.wafers$
+        .pipe(map((w) => w.find((w) => w.id === id))) // w => this.http.get<wafer>(`${this.url}/${id})
+        .pipe(catchError(this.handleError))
+    )
+  );
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage = `An error occured: ${err.error.message}`;
+    console.error(err);
+    return throwError(() => errorMessage);
+  }
+}
